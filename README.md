113e9fea963f9dfe3f4b1ad6f16ccb823b90067f
Ian Holmes <ihh@berkeley.edu>
2016-01-13 11:48:34 -0800

    rename propaelor -> propalor (string constants)

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# propaelor
+# propalor
 Protein Paleogenetic Reconstruction
--- t/propaelor.cpp
+++ t/propaelor.cpp
@@ -9,26 +9,26 @@
 #include "../src/optparser.h"
 
 // GNU --version
-#define PROPAELOR_PROGNAME "propaelor"
-#define PROPAELOR_VERSION  "0.1"
+#define PROPALOR_PROGNAME "propalor"
+#define PROPALOR_VERSION  "0.1"
 
-struct PropaelorUsage : OptParser {
-  PropaelorUsage (int argc, char** argv);
+struct PropalorUsage : OptParser {
+  PropalorUsage (int argc, char** argv);
 };
 
-PropaelorUsage::PropaelorUsage (int argc, char** argv)
-  : OptParser (argc, argv, PROPAELOR_PROGNAME, "{help,version} [options]")
+PropalorUsage::PropalorUsage (int argc, char** argv)
+  : OptParser (argc, argv, PROPALOR_PROGNAME, "{help,version} [options]")
 {
 }
 
 int main (int argc, char** argv) {
 
   try {
-    PropaelorUsage usage (argc, argv);
+    PropalorUsage usage (argc, argv);
   
     const string command = usage.getCommand();
 
-    return usage.parseUnknownCommand (command, PROPAELOR_VERSION);
+    return usage.parseUnknownCommand (command, PROPALOR_VERSION);
 
   } catch (...) {
     return EXIT_FAILURE;
