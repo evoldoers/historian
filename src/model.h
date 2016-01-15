106a221e8002732073dd1fe8cb67e90170110513
Ian Holmes <ihh@berkeley.edu>
2016-01-15 10:30:17 -0800

    model.h

---
+++ src/model.h
@@ -0,0 +1,38 @@
+#ifndef MODEL_INCLUDED
+#define MODEL_INCLUDED
+
+#include <gsl/gsl_matrix.h>
+#include <string>
+#include "gason.h"
+
+using namespace std;
+
+struct RootModel {
+  gsl_vector init;
+  double extend, end;  // Strict (geometric) normalization: end = 1 - extend. Weak (flat): end = extend = 1
+};
+
+struct RateModel {
+  double insRate, delRate, insExtProb, delExtProb;
+  gsl_matrix subRate;
+
+  void read (const JsonValue& json);
+};
+
+struct RateModelBasis {
+  const RateModel& model;
+  gsl_vector eqm;
+  gsl_vector eval;
+  gsl_matrix evec;
+  RateModelBasis (const RateModel& model);
+  RootModel defaultRootModel() const;
+  gsl_matrix exp (double t) const;
+};
+
+struct ProbModel {
+  double ins, del, insExt, delExt;
+  gsl_matrix sub;
+  ProbModel (const RateModelBasis& basis, double t);
+};
+
+#endif /* MODEL_INCLUDED */
