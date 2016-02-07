0a2f7a8bae7d7f1b251489f07b6f936a27bcc56b
Ian Holmes <ihh@berkeley.edu>
2016-02-06 22:02:23 -0800

    -ancseq option to reconstruct actual ancestral sequences (edited help text)

--- t/historian.cpp
+++ t/historian.cpp
@@ -57,7 +57,7 @@ ProgUsage::ProgUsage (int argc, char** argv)
     + "                  Save various intermediate analysis results to files\n"
     + "\n"
     + "Reconstruction algorithm options:\n"
-    + "  -ancseq         Reconstruct ancestral sequences (default leaves them as *'s)\n"
+    + "  -ancseq         Predict ancestral sequences (default is to leave them as *'s)\n"
     + "  -band <n>       Size of band around guide alignment (default " + to_string(DefaultMaxDistanceFromGuide) + ")\n"
     + "  -noband         Turn off band, ignore guide alignment\n"
     + "  -minpost <p>    Posterior prob. threshold for profile states (default " + TOSTRING(DefaultProfilePostProb) + ")\n"
