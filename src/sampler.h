7dcffc2ea0349f6f0ff4a39737d13697808fbef8
Ian Holmes <ihholmes@gmail.com>
2016-04-25 19:39:54 -0700

    more prototype sketching for MCMC

--- src/sampler.h
+++ src/sampler.h
@@ -17,17 +17,38 @@ struct Sampler {
   };
 
   struct Move {
+    enum Type { Branch, TripleBranch, PruneRegraft, NodeHeight };
+    Type type;
+    TreeNodeIndex node, parent, leftChild, rightChild, oldSibling, newSibling;
     State oldState, newState;
-    LogProb logHastingsRatio;
+    LogProb logProposal, logInverseProposal, logOldState, logNewState, logHastingsRatio;
   };
+
+  struct AlignmentProposal {
+    const RateModel* model;
+    FastSeq seq0, seq1;
+    TreeBranchLength dist;
+    AlignPath path;
+    LogProb logPostProb;  // log P(path|model,seq0,seq1,dist)
+
+    AlignmentProposal (const RateModel* model, const FastSeq& seq0, const FastSeq& seq1, TreeBranchLength dist);
+    AlignmentProposal (const AlignPath& path, const RateModel* model, const FastSeq& seq0, const FastSeq& seq1, TreeBranchLength dist);
+  };
+
+  struct ParentProposal {
+    const RateModel* model;
+    TreeNodeIndex lNode, rNode, pNode;
+    FastSeq lSeq, rSeq, pSeq;
+    TreeBranchLength plDist, prDist;
+    AlignPath plrPath;
+    LogProb logPostProb;  // log P(pSeq,plrPath|model,lSeq,rSeq,plDist,prDist,lrPath)
+
+    ParentProposal (const RateModel* model, const FastSeq& lSeq, const FastSeq& rSeq, TreeBranchLength plDist, TreeBranchLength prDist, const AlignPath& lrPath);
+    ParentProposal (const FastSeq& pSeq, const AlignPath& plrPath, const RateModel* model, const FastSeq& lSeq, const FastSeq& rSeq, TreeBranchLength plDist, TreeBranchLength prDist, const AlignPath& lrPath);
+};
   
   RateModel model;
   SimpleTreePrior treePrior;
-
-  Move realignBranch (const State& oldState, TreeNodeIndex childNode) const;
-  Move realignNodeNeighborhood (const State& oldState, TreeNodeIndex node) const;
-  Move pruneAndRegraft (const State& oldState, TreeNodeIndex childNode, TreeNodeIndex newSibling) const;
-  Move resampleNodeHeight (const State& oldState, TreeNodeIndex node) const;
 };
 
 #endif /* SAMPLER_INCLUDED */
