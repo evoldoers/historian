7475ad9b6a6ea0b0c30d9bb00fe3690e2cc8f860
Ian Holmes <ihh@berkeley.edu>
2016-10-18 10:38:00 -0700

    Variable branch lengths

--- doc/sim/bin/add-indelign-params.js
+++ doc/sim/bin/add-indelign-params.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 
 var fs = require("fs")
-var len = process.argv[2], maxIndelLen = process.argv[3], indelProb = process.argv[4], treeFile = process.argv[5]
+var len = process.argv[2], maxIndelLen = process.argv[3], indelProb = process.argv[4], treeFile = process.argv[5], branchMultiplier = process.argv[6] || 1
 var tree = fs.readFileSync (treeFile, 'utf8')
+
+tree = tree.replace (/:([\d\.]+)/g, function (str, p1, offset, s) {
+    return ':' + (p1 * branchMultiplier)
+})
+
 process.stdout.write ("[" + len + "]{" + maxIndelLen + "," + indelProb + "}" + tree)
