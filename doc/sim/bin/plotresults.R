c53cec6f0e692c8c12a7c95d1d406b7ba308bdfd
Ian Holmes <ihh@berkeley.edu>
2016-11-01 15:54:30 -0700

    Updated

--- doc/sim/bin/plotresults.R
+++ doc/sim/bin/plotresults.R
@@ -13,15 +13,25 @@ data.all <- read.csv(file="gp120sim/results.1-100.dat",sep=" ")
 data.sym <- data.all[data.all$tree=="sym",]
 data.tree <- data.all[data.all$tree=="tree",]
 
+quartiles.all <- read.csv(file="gp120sim/quartiles.1-100.dat",sep=" ")
+quartiles.sym <- quartiles.all[quartiles.all$tree=="sym",]
+quartiles.tree <- quartiles.all[quartiles.all$tree=="tree",]
+
 simplot <- function(method,title) {
 	rms <- rms.tree
 	data <- data.tree
+	quartiles <- quartiles.tree
 	err.mean <- rms$insdelmean[rms$method==method]
 	err.rms <- rms$insdelrmse[rms$method==method]
+	err.lq <- quartiles$lower[quartiles$method==method]
+	err.uq <- quartiles$upper[quartiles$method==method]
+	err.median <- quartiles$median[quartiles$method==method]
 	return (ggplot(data[data$method==method,],aes(x=bin,weight=count))
 		+geom_bar()
-		+annotate("text",label=paste0(title,"\nMean error: ",sprintf("%.2f",round(err.mean-1,digits=2)),"\nRMS error: ",sprintf("%.2f",round(err.rms,digits=2))),x=1.6,y=1.75)
-		+xlab("Estimated rate / true rate")
+		+annotate("text",label=paste0(title,"\nMean error: ",sprintf("%.2f",round(err.mean-1,digits=2)),"\nRMS error: ",sprintf("%.2f",round(err.rms,digits=2))),x=1.6,y=1.75,size=3)
+		+geom_segment(aes(y=3,yend=3,x=err.lq,xend=err.uq))
+		+annotate("point",y=3,x=err.median)
+		+xlab("Estimated indel rate / true indel rate")
 		+scale_x_continuous(limits = c(0.25,1.75))
 		+scale_y_continuous(limits = c(0,3))
 		+theme(axis.title.y=element_blank(),
@@ -30,10 +40,13 @@ simplot <- function(method,title) {
 }
 
 ma <- simplot("ma.tree","(a) True alignment")
-hist <- simplot("histslow","(b) Historian")
-prank <- simplot("prank","(c) Prank")
-prob <- simplot("probcons","(d) ProbCons")
-muscle <- simplot("muscle","(e) Muscle")
+histslow <- simplot("histslow","(b) Historian (default)")
+hist <- simplot("hist","(c) Historian (fast)")
+prank <- simplot("prank","(d) Prank")
+prob <- simplot("probcons","(e) ProbCons")
+muscle <- simplot("muscle","(f) Muscle")
 
-g <- arrangeGrob(ma,hist,prank,prob,muscle,ncol=1)
+g <- arrangeGrob(ma,histslow,hist,prank,prob,muscle,ncol=1)
 ggsave("results.pdf",g)
+
+warnings()
--- doc/sim/bin/results.js
+++ doc/sim/bin/results.js
@@ -5,16 +5,30 @@ var fs = require("fs");
 var binsize = process.argv[2],
     binmax = process.argv[3],
     nbins = (binmax/binsize)+1,
-    togetherFlag = eval(process.argv[4]),
+    quartileFlag = eval(process.argv[4]),
     methods = process.argv[5].split(" "),
     dirs = process.argv.slice(6)
 
-function incbin(r,bins) {
+function incbin(r,bins,vals) {
     var bin = Math.round(r/binsize)
     bins[bin] = (bins[bin] || 0) + 1
+    vals.push(r)
 }
 
+process.stdout.write ((quartileFlag
+                       ? ["method","tree","lower","median","upper"]
+                       : ["event","method","rate","tree","multiplier","bin","count"])
+                      .join (" ") + "\n")
+
 var binStart = Array.apply(null,Array(nbins)).map (function(x,n) { return (n*binsize).toPrecision(2) })
+function printrange(method,tree,vals) {
+    var sortedVals = vals.sort (function (a,b) { return a - b })
+    var len = sortedVals.length
+    var lq = sortedVals[Math.floor(len/4)], median = sortedVals[Math.floor(len/2)], uq = sortedVals[Math.floor(3*len/4)]
+    process.stdout.write ([method,tree,lq,median,uq]
+                          .join(" ") + "\n")
+}
+
 function printbins(label,headers,bins) {
     var total = 0
     Object.keys(bins).forEach (function (n) {
@@ -28,40 +42,41 @@ function printbins(label,headers,bins) {
     })
 }
 
-process.stdout.write (["event","method"]
-                      .concat (togetherFlag ? [] : ["rate","tree","multiplier"])
-                      .concat(["bin","count\n"])
-                      .join (" "))
-
 methods.forEach (function(method) {
-    var ibins = {}, dbins = {}
+    var ibins = {}, dbins = {}, vals = {}
     dirs.forEach (function(dir) {
-        var match = /.*rate([\d\.]+)\/([a-z]+)(\d+)/.exec(dir),
-            rate = match[1],
-            tree = match[2],
-            mul = match[3],
-            filenameRegex = new RegExp('\\d+\\.' + method + '\\.json'),
-            headers
-        fs.readdirSync(dir)
-            .filter (function (filename) { return filenameRegex.test(filename) })
-            .map (function (filename) {
-                try {
-                    var json = JSON.parse(fs.readFileSync(dir+"/"+filename));
-                    incbin(json.insrate/rate,ibins);
-                    incbin(json.delrate/rate,dbins)
-                } catch(err) {
-                    process.stderr.write(err.toString()+" ("+dir+"/"+filename+")\n")
-                }
-            })
-        if (!togetherFlag) {
-            printbins("ins",[method,rate,tree,mul],ibins)
-            printbins("del",[method,rate,tree,mul],dbins)
-            ibins = {}
-            dbins = {}
+        var match = /.*rate([\d\.]+)\/([a-z]+)(\d+)/.exec(dir)
+        if (match) {
+            var rate = match[1],
+                tree = match[2],
+                mul = match[3],
+                filenameRegex = new RegExp('\\d+\\.' + method + '\\.json'),
+                headers
+            if (!vals[tree])
+                vals[tree] = []
+            fs.readdirSync(dir)
+                .filter (function (filename) { return filenameRegex.test(filename) })
+                .map (function (filename) {
+                    try {
+                        var json = JSON.parse(fs.readFileSync(dir+"/"+filename))
+                        incbin(json.insrate/rate,ibins,vals[tree])
+                        incbin(json.delrate/rate,dbins,vals[tree])
+                    } catch(err) {
+                        process.stderr.write(err.toString()+" ("+dir+"/"+filename+")\n")
+                    }
+                })
+            if (!quartileFlag) {
+                printbins("ins",[method,rate,tree,mul],ibins)
+                printbins("del",[method,rate,tree,mul],dbins)
+                ibins = {}
+                dbins = {}
+            }
         }
     })
-    if (togetherFlag) {
-        printbins("ins",[method],ibins)
-        printbins("del",[method],dbins)
+    if (quartileFlag) {
+        Object.keys(vals).forEach (function (tree) {
+            printrange(method,tree,vals[tree])
+        })
     }
 })
+
