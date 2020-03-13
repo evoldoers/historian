3b6dcd1b7308b252b2a0c47e1dae5f21ba8452c4
Ian Holmes <ihh@berkeley.edu>
2020-03-12 18:19:43 -0700

    Web worker stub

--- emcc/pre.js
+++ emcc/pre.js
@@ -1,9 +1,17 @@
 // This code goes at the start of the generated historian JS
 Module.noInitialRun = true;
 Module.noExitRuntime = true;
+
 Module.print = function (x) {
   if (Module.stdout)
     Module.stdout.push (x)
+  else
+    console.log (x)
+}
+
+Module.printErr = function (x) {
+  if (Module.stderr)
+    Module.stderr.push (x)
   else
     console.log (x)
 }
@@ -32,15 +40,28 @@ Module.runWithFiles = (args) => {
 	  return opt
       })
 
+      const oldStdout = Module.stdout
+      const oldStderr = Module.stderr
+      
       Module.stdout = []
+      Module.stderr = []
       Module.callMain (wrappedArgs)
       const stdout = Module.stdout.join('')
-      delete Module.stdout
+      const stderr = Module.stderr.join('')
+      Module.stdout = oldStdout
+      Module.stderr = oldStderr
 
-      let result = { stdout, outputs }
+      let file = {}
       outputs.forEach ((filename) => {
-        result[filename] = Module.FS.readFile (filename, { encoding: 'utf8'})
+        file[filename] = Module.FS.readFile (filename, { encoding: 'utf8'})
       })
-      return result
+      return { stdout, stderr, outputs, file }
     })
+}
+
+// Web worker
+onmessage = function(e) {
+  Module.runWithFiles(e.data).then ((result) => {
+    postMessage (result);
+  })
 }
