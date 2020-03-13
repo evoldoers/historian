089c93f09f8a6ec629659ec148101e55e92900b1
Ian Holmes <ihh@berkeley.edu>
2020-03-12 17:38:59 -0700

    wasm

--- emcc/pre.js
+++ emcc/pre.js
@@ -1,4 +1,4 @@
-// This code goes at the start of the generated boss JS
+// This code goes at the start of the generated historian JS
 Module.noInitialRun = true;
 Module.noExitRuntime = true;
 Module.print = function (x) {
@@ -6,4 +6,41 @@ Module.print = function (x) {
     Module.stdout.push (x)
   else
     console.log (x)
+}
+
+Module.runtimeInitPromise = new Promise ((resolve, reject) => {
+  Module.onRuntimeInitialized = resolve
+})
+
+Module.runWithFiles = (args) => {
+  return Module.runtimeInitPromise
+    .then (() => {
+      args = args || [];
+
+      let nFiles = 0, filePrefix = 'FILE', outputs = []
+      const wrappedArgs = args.map ((opt) => {
+	if (typeof(opt) !== 'string') {
+	  const filename = opt.filename || (filePrefix + (++nFiles))
+          if (opt.input || opt.data) {
+            const fileBuffer = Buffer.from (opt.data || '', 'utf-8')
+	    Module.FS.writeFile (filename, new Uint8Array(fileBuffer))
+          }
+          if (opt.output)
+            outputs.push (filename)
+	  return filename
+	} else
+	  return opt
+      })
+
+      Module.stdout = []
+      Module.callMain (wrappedArgs)
+      const stdout = Module.stdout.join('')
+      delete Module.stdout
+
+      let result = { stdout, outputs }
+      outputs.forEach ((filename) => {
+        result[filename] = Module.FS.readFile (filename, { encoding: 'utf8'})
+      })
+      return result
+    })
 }
